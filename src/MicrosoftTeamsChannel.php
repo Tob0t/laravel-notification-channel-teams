a66e1cb0e07bbba2897d6256e8346322a9cdf4ae
Tobias Madner <tobias.madner@pinpoll.com>
2020-04-16 19:12:25 +0200

    remove unused import

--- src/MicrosoftTeamsChannel.php
+++ src/MicrosoftTeamsChannel.php
@@ -3,7 +3,6 @@
 namespace NotificationChannels\MicrosoftTeams;
 
 use Illuminate\Notifications\Notification;
-use NotificationChannels\MicrosoftTeams\Exceptions\CouldNotSendNotification;
 
 class MicrosoftTeamsChannel
 {
